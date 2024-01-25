9730942f4d4fc16810f0307be42b72f7d1b192cd
AlejandroLoutphi <alejandroloutphi@gmail.com>
2024-01-25 18:32:50 -0400

    Replaced lambda with def for codigo_morse_list_comp.py

--- codigo_morse_list_comp.py
+++ codigo_morse_list_comp.py
@@ -1,10 +1,13 @@
+def list_translate(item, input_list: list, output_list: list):
+    return output_list[input_list.index(item)]
+
 letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", ".", ",", "?", "\'", "/", ":", ";", "+", "-", "=", " "]
 morse = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.","--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", "-----", ".-.-.-", "---..--", "..--..", ".----.", "-..-.", "---...", "-.-.-.", ".-.-.", "-....-", "-...-", "/"]
 
 x = input("Inserta el texto que desea traducir a codigo morse\n")
 x = x.lower()
 try:
-    x = [(lambda item: morse[letters.index(item)])(i) for i in x]
+    x = [list_translate(i, letters, morse) for i in x]
 except:
     print("Este texto no se puede traducir a código morse")
     quit()
